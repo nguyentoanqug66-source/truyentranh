e60fff6fc85b9701f4023d7a396f6bc8b22c7682
nguyentoanqug66-source <nguyentoanqug66@gmail.com>
2026-03-15 23:08:34 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 const SUPABASE_URL = "https://hfnftzycxlkkoilpcssr.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_9jUO5VwzOulx_aqeuBmFug_FuQLFHsD";
+
+window.SUPABASE_URL = SUPABASE_URL;
+window.SUPABASE_ANON_KEY = SUPABASE_ANON_KEY;
